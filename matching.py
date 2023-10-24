8cff0ffdb950a65cfcc8c17e203b59656125b490
marius schlueter <themrslue@googlemail.com>
2023-10-24 23:53:50 +0200

    added new mincostalgo

--- matching.py
+++ matching.py
@@ -1,15 +1,8 @@
 import pandas as pd
 import openpyxl
-import numpy as np
-from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import maximum_flow
-import networkx as nx
-import matplotlib.pyplot as plt
-
-DAYS = ['mon', 'tue', 'wed', 'thr', 'fri', 'sat', 'sun']
-SHIFTS = ['9', '14', '18']
-SHIFT_TYPES = ["cut", "glu", "san", "che", "vab", "lam", "cor"]
-
+from general import DAYS,SHIFTS,SHIFT_TYPES
+from maxflow import max_flow_matching
+from mincost import min_cost_matching
 
 def read_sheet(file="Shiftsplan_lux025.xlsx", sheet="ShiftList_KW16"):
     dataframe = openpyxl.load_workbook(file)
@@ -72,79 +65,6 @@ def read_sheet(file="Shiftsplan_lux025.xlsx", sheet="ShiftList_KW16"):
     return df, shift_type_dict
 
 
-def shiftplan_to_adj_mat(df):
-    n_nodes = 1 + df.shape[0] + (len(SHIFTS) * len(DAYS)) + 1
-    graph_matrix = np.zeros((n_nodes, n_nodes), dtype="int64")
-    graph_matrix[0, 1:(1 + df.shape[0])] = df["n_shifts"].to_numpy()
-    graph_matrix[(1 + df.shape[0]):-1, -1] = 3
-    for i, d in enumerate(DAYS):
-        for j, s in enumerate(SHIFTS):
-            key = d + "_" + s
-            graph_matrix[1:(1 + df.shape[0]), (1 + df.shape[0] + (i * len(SHIFTS)) + j)] = df[key].to_numpy()
-    return graph_matrix
-
-
-def make_sl_adj_mat(df, graph_matrix_sl, shift_type_dict):
-    graph_matrix_sl = graph_matrix_sl.copy()
-
-    for s in shift_type_dict:
-        df_sl = None
-        for st in shift_type_dict[s]:
-            if df_sl is None:
-                df_sl = df["sl_" + st].to_numpy().copy()
-            else:
-                df_sl |= df["sl_" + st].to_numpy()
-        if df_sl is None:
-            df_sl = np.array(df.shape[0] * [False])
-        keysplit = s.split('_')
-        column_index = DAYS.index(keysplit[0]) * 3 + SHIFTS.index(keysplit[1])
-        graph_matrix_sl[1:(1 + df.shape[0]), 1 + df.shape[0] + column_index][df_sl.transpose() == False] = 0
-
-    graph_matrix_sl[(1 + df.shape[0]):-1, -1] = 1
-
-    return graph_matrix_sl
-
-
-def make_worker_adj_mat(df, graph_matrix, flow_sl, max_people_at_shift=3):
-    graph_matrix = graph_matrix.copy()
-    flow_shift_counts = flow_sl.flow.toarray()[(1 + df.shape[0]):-1, -1]
-    flow_shift_counts[flow_shift_counts > 0] = max_people_at_shift
-    graph_matrix[(1 + df.shape[0]):-1, -1] = flow_shift_counts
-    sl_flow_only_pos = flow_sl.flow.toarray().copy()
-    sl_flow_only_pos[sl_flow_only_pos < 0] = 0
-    graph_matrix = graph_matrix - sl_flow_only_pos
-
-    return graph_matrix
-
-
-def get_flow_from_adj_mat(adj_mat):
-    graph_mat = csr_matrix(adj_mat)
-    flow = maximum_flow(graph_mat, 0, adj_mat.shape[0] - 1)
-    return flow
-
-
-def get_shift_list(df, flow_sl, flow, add_flow=None):
-    np_flow = flow.flow.toarray()
-    np_sl_flow = flow_sl.flow.toarray()
-    shift_list = {}
-    for i, day in enumerate(DAYS):
-        for j, shift in enumerate(SHIFTS):
-            key = day + "_" + shift
-            ind_people = np.where(np_flow[1:(1 + df.shape[0]), (1 + df.shape[0] + (i * len(SHIFTS)) + j)] == 1)
-            ind_sl = np.where(np_sl_flow[1:(1 + df.shape[0]), (1 + df.shape[0] + (i * len(SHIFTS)) + j)] == 1)
-            shift_list[key] = {"lead": None, "worker": []}
-            if len(ind_sl[0]):
-                shift_list[key]["lead"] = df["name"][ind_sl[0][0]]
-            for k in list(ind_people[0]):
-                shift_list[key]["worker"].append(df['name'][k])
-            if add_flow is not None:
-                ind_people_2 = np.where(
-                    add_flow.flow.toarray()[1:(1 + df.shape[0]), (1 + df.shape[0] + (i * len(SHIFTS)) + j)] == 1)
-                for k in list(ind_people_2[0]):
-                    shift_list[key]["worker"].append(df['name'][k])
-    return shift_list
-
-
 def get_beer_list(df):
     obj = {}
     beers = df[df["beer"] > 0]
@@ -152,48 +72,6 @@ def get_beer_list(df):
     for n in list(beers['name']):
         obj[n] = df[df["name"] == n]["beer"]
     return obj
-
-
-def draw_matching(df, adj_mat, out_file=None):
-    G = nx.from_numpy_array(adj_mat)
-
-    node_labels = ["start"]
-    node_labels += list(df['name'])
-    node_labels += [d + '_' + s for d in DAYS for s in SHIFTS]
-    node_labels += ["end"]
-
-    node_labels_obj = {}
-
-    for i, v in enumerate(node_labels):
-        node_labels_obj[i] = v
-
-    G = nx.relabel_nodes(G, node_labels_obj)
-
-    pos = []
-
-    pos.append((0, 40))
-
-    for i in range(1, 1 + df.shape[0]):
-        pos.append((2, i - 1))
-
-    for i in range(1 + df.shape[0], 1 + df.shape[0] + (len(SHIFTS) * len(DAYS))):
-        pos.append((4, (i - 1 - df.shape[0]) * 4))
-
-    pos.append((6, 40))
-
-    pos_obj = {}
-
-    for i, v in enumerate(pos):
-        pos_obj[node_labels[i]] = v
-
-    plt.figure(figsize=(15, 40))
-    # labels = nx.get_edge_attributes(G,'weight')
-    # nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-    nx.draw_networkx(G, arrows=True, pos=pos_obj)
-    if out_file is None:
-        plt.show()
-    else:
-        plt.savefig(out_file)
 
 
 def main():
@@ -208,7 +86,8 @@ def main():
 
     layout = [[sg.Column([[sg.Text("Excel file:")], [sg.Text("Sheet name:")]]),
                sg.Column([[sg.InputText(), sg.FileBrowse()], [sg.InputText("ShiftList_KW16")]])],
-              [sg.Text("Maximum shift size:"), sg.InputText("3", key="-MAX-SHIFT-SIZE-", size=3)],
+              [sg.Text("Maximum shift size:"), sg.InputText("3", key="-MAX-SHIFT-SIZE-", size=3), 
+               sg.Checkbox("Use new Algorithm", default=True, key="-USE-NEW-ALGO-")],
               [sg.Button('Generate Shiftplan'), sg.Button('Generate Beerlist'), sg.Text("Comment:"),
                sg.InputText(key="-COMMENT-", size=25)],
               [sg.Multiline(size=(70, 15), key="-OUTBOX-")]]
@@ -255,57 +134,15 @@ def main():
             max_shift_size = int(values["-MAX-SHIFT-SIZE-"])
             print("Generate Matching for File:", fname, " Worksheet: ", sname)
             df, shift_type_dict = read_sheet(fname, sname)
-            base_mat = shiftplan_to_adj_mat(df)
 
-            shift_degree = [df[s].sum() for s in shift_type_dict]
-            len([x for x in shift_degree if x < 2])
-            for i,key in enumerate(shift_type_dict):
-                if shift_degree[i] < 2:
-                    shift_type_dict[key] = []
-
-            # Find the best flow, that results in the most shifts possible.
-
-            best_sl_flow = None
-            best_w_mat = None
-            best_flow = None
-            best_flow_sum_value = 0
-            best_df = None
-
-            for _ in range(50):
-                df = df.sample(frac=1).reset_index(drop=True)
-                base_mat = shiftplan_to_adj_mat(df)
-                shift_type_dict_copy = shift_type_dict.copy()
-                for _ in range(10):
-                    sl_mat = make_sl_adj_mat(df, base_mat, shift_type_dict_copy)
-                    flow_sl = get_flow_from_adj_mat(sl_mat)
-                    w_mat = make_worker_adj_mat(df, base_mat, flow_sl, 2)
-                    flow = get_flow_from_adj_mat(w_mat)
-
-                    shift_list = get_shift_list(df, flow_sl, flow)
-
-                    print(flow_sl.flow_value + flow.flow_value)
-
-                    for k in shift_list:
-                        if (shift_list[k]["lead"] is not None) and (
-                                shift_list[k] is None or len(shift_list[k]["worker"]) == 0):
-                            shift_type_dict_copy[k] = []
-                            shift_list = None
-                            break
-
-                    if shift_list is not None:
-                        break
-
-                if (flow_sl.flow_value + flow.flow_value) > best_flow_sum_value:
-                    best_sl_flow = flow_sl
-                    best_w_mat = w_mat
-                    best_flow = flow
-                    best_flow_sum_value = (flow_sl.flow_value + flow.flow_value)
-                    best_df = df.copy()
-
-            w_mat_2 = make_worker_adj_mat(best_df, best_w_mat, best_flow, max_shift_size - 1)
-            flow_2 = get_flow_from_adj_mat(w_mat_2)
-            shift_list = get_shift_list(best_df, best_sl_flow, best_flow, flow_2)
-            print(best_sl_flow.flow_value + best_flow.flow_value + flow_2.flow_value)
+            if values["-USE-NEW-ALGO-"]:
+                if max_shift_size != 3:
+                    sg.popup("the new algorithm is only implemented for max shiftsize of 3")
+                else:
+                    window["-OUTBOX-"].update("Please wait. The new Algorithm is slower :)")
+                    shift_list = min_cost_matching(df,shift_type_dict)
+            else:
+                shift_list = max_flow_matching(df,shift_type_dict,max_shift_size)
 
             sl_str = ""
             for k in shift_list:
---
+++ mincost.py
@@ -0,0 +1,222 @@
+import numpy as np
+from functools import reduce
+import networkx as nx
+from general import DAYS, SHIFTS, get_shift_index
+
+class MatchingGraph:
+
+    def __init__(self,df,shift_type_dict,copy_instance=None):
+
+        if copy_instance is not None:
+            self.graph = copy_instance.graph.copy()
+            self.node_offsets = copy_instance.node_offsets
+            self.node_lengths = copy_instance.node_lengths
+            self.df = copy_instance.df
+            return
+        
+        assert copy_instance is None
+
+        shift_degree = [df[s].sum() for s in shift_type_dict]
+        len([x for x in shift_degree if x < 2])
+        for i,key in enumerate(shift_type_dict):
+            if shift_degree[i] < 2:
+                shift_type_dict[key] = []
+
+        node_lengths = [1,
+                        df.shape[0],
+                        (df.shape[0] * len(SHIFTS) * len(DAYS)),
+                        (len(SHIFTS) * len(DAYS)),
+                        (len(SHIFTS) * len(DAYS)),
+                        (len(SHIFTS) * len(DAYS)),
+                        1]
+        node_offsets = [reduce(lambda x,y: x+y, node_lengths[:i]) for i in range(1,len(node_lengths))]
+        print(node_offsets)
+        n_nodes = reduce(lambda x,y: x+y, node_lengths)
+        graph_matrix = np.zeros((n_nodes, n_nodes), dtype="int64")
+        graph_matrix[0, node_offsets[0]:node_offsets[1]] = df["n_shifts"].to_numpy()
+        #graph_matrix[node_offsets[1]:node_offsets[2], -1] = 3
+
+        shiftlead_vector = np.zeros(df.shape[0]*len(SHIFTS)*len(DAYS))
+        stretch_factor = len(SHIFTS)*len(DAYS)
+
+        for s in shift_type_dict:
+            df_sl = None
+            for st in shift_type_dict[s]:
+                if df_sl is None:
+                    df_sl = df["sl_" + st].to_numpy().copy()
+                else:
+                    df_sl |= df["sl_" + st].to_numpy()
+            if df_sl is None:
+                df_sl = np.array(df.shape[0] * [False])
+            keysplit = s.split('_')
+            column_index = DAYS.index(keysplit[0]) * 3 + SHIFTS.index(keysplit[1])
+            shiftlead_vector[column_index::stretch_factor] = df_sl
+            graph_matrix[node_offsets[1]:node_offsets[2],node_offsets[2]+column_index] = shiftlead_vector
+
+        for index, row in df.iterrows():
+            avai = row.to_numpy()[3:3+(len(SHIFTS)*len(DAYS))]
+            start_col = node_offsets[1]+(index*len(SHIFTS)*len(DAYS))
+            end_col = node_offsets[1]+((index+1)*len(SHIFTS)*len(DAYS))
+            graph_matrix[node_offsets[0]+index, start_col:end_col] = avai
+            #graph_matrix[start_col:end_col, node_offsets[2]:node_offsets[3]] = np.identity(len(SHIFTS)*len(DAYS))
+            graph_matrix[start_col:end_col, node_offsets[3]:node_offsets[4]] = np.identity(len(SHIFTS)*len(DAYS))
+            graph_matrix[start_col:end_col, node_offsets[4]:node_offsets[5]] = np.identity(len(SHIFTS)*len(DAYS))
+
+
+        graph_matrix[node_offsets[4]:node_offsets[5], 0] = 1
+        graph_matrix[0,node_offsets[5]] = 1000
+
+        edge_costs={}
+        for i in range(n_nodes):
+            for j in range(n_nodes):
+                if j == 0 and node_offsets[4] <= i < node_offsets[5]:
+                    edge_costs[i,j] = -1
+                else:
+                    edge_costs[i,j] = 0
+
+        node_demands=np.zeros((n_nodes),dtype="int16")
+        for i in range(n_nodes):
+            if i == 0:
+                node_demands[i] = -1000
+            elif node_offsets[2] <= i < node_offsets[4]:
+                node_demands[i] = 1
+            elif i == node_offsets[5]:
+                node_demands[i] = 1000 - (2*len(SHIFTS)*len(DAYS))
+            else:
+                node_demands[i] = 0
+
+        # disable shifts
+        def __disable_shift(i):
+            node_demands[node_offsets[2]+i] = 0
+            node_demands[node_offsets[3]+i] = 0
+            node_demands[0] += 2
+            graph_matrix[:,node_offsets[4]+i] = 0
+
+        for s in shift_type_dict:
+            keysplit = s.split('_')
+            column_index = DAYS.index(keysplit[0]) * 3 + SHIFTS.index(keysplit[1])
+            if len(shift_type_dict[s]) == 0:
+                __disable_shift(column_index)
+                print("disabled shift",s)
+
+        graph = nx.from_numpy_array(graph_matrix, create_using=nx.DiGraph())
+
+        nx.set_edge_attributes(graph, edge_costs, "cost")
+
+        node_demands_dict = dict(zip(range(len(node_demands)),list(node_demands),))
+        nx.set_node_attributes(graph, node_demands_dict, "demand")
+
+        self.graph = graph
+        self.node_offsets = node_offsets
+        self.node_lengths = node_lengths
+        self.df = df
+
+    def get_flow(self):
+        try:
+            flow = nx.min_cost_flow(self.graph,capacity="weight",weight="cost")
+            return flow
+        except nx.NetworkXUnfeasible:
+            print("no flow found")
+            return None
+
+    def get_flow_value(self):
+        try:
+            flow = nx.min_cost_flow_cost(self.graph,capacity="weight",weight="cost")
+            return flow
+        except nx.NetworkXUnfeasible:
+            print("no flow found")
+            return None
+
+    def disable_shift(self,i):
+        self.graph.nodes[self.node_offsets[2]+i]["demand"] = 0
+        self.graph.nodes[self.node_offsets[3]+i]["demand"] = 0
+        self.graph.nodes[0]["demand"] = self.graph.nodes[0]["demand"] + 2
+        for j in range(self.node_offsets[1],self.node_offsets[2]):
+            try:
+                self.graph.remove_edge(j,self.node_offsets[4]+i)
+            except:
+                pass
+
+    def get_best_flow_by_remove_shift(self,avai_shifts,recursion_level=0):
+        best_flow_shift_combi = None
+        best_flow_shift_value = 1000
+        for d in DAYS:
+            for s in SHIFTS:
+                column_index = DAYS.index(d) * 3 + SHIFTS.index(s)
+                
+                if column_index not in avai_shifts:
+                    continue
+
+                graph_copy = self.copy()
+                graph_copy.disable_shift(column_index)
+
+                if recursion_level == 0:
+                    flow_combi = []
+                    flow_value = graph_copy.get_flow_value()
+                    print("0 try",d,s,"value",flow_value)
+                else:
+                    print(recursion_level,"try",d,s)
+                    avai_shifts_copy = avai_shifts.copy()
+                    avai_shifts_copy.remove(column_index)
+                    retval = graph_copy.get_best_flow_by_remove_shift(avai_shifts_copy,recursion_level-1)
+                    if retval is not None:
+                        flow_combi = retval[0]
+                        flow_value = retval[1]
+                    else:
+                        flow_combi = None
+                        flow_value = None
+
+                if flow_combi is not None and flow_value is not None and best_flow_shift_value > flow_value:
+                    best_flow_shift_value = flow_value
+                    best_flow_shift_combi = flow_combi + [d+"_"+s]
+
+        if best_flow_shift_combi is not None:
+            return  best_flow_shift_combi,best_flow_shift_value
+
+    def get_matched_person(self,flow,shift,position):
+        shift_ind = get_shift_index(shift)
+        for i in range(self.df.shape[0]*len(DAYS)*len(SHIFTS)):
+            if (self.node_offsets[position]+shift_ind) in flow[i+self.node_offsets[1]] and flow[i+self.node_offsets[1]][self.node_offsets[position]+shift_ind] > 0:
+                return (self.df.iloc[i//(len(DAYS)*len(SHIFTS))]["name"])
+            
+    def get_shift_list(self,flow):
+        shift_list = {}
+        for day in DAYS:
+            for shift in SHIFTS:
+                key = day + "_" + shift
+                shift_list[key] = {"lead": None, "worker": []}
+                lead = self.get_matched_person(flow,key,2)
+                w1   = self.get_matched_person(flow,key,3)
+                w2   = self.get_matched_person(flow,key,4)
+                if lead != None:
+                    shift_list[key]["lead"] = lead
+                if w1 != None:
+                    shift_list[key]["worker"].append(w1)
+                if w2 != None:
+                    shift_list[key]["worker"].append(w2)
+        return shift_list
+    
+    def __copy__(self):
+        return MatchingGraph(None,None,copy_instance=self)
+    
+    def copy(self):
+        return self.__copy__()
+
+def min_cost_matching(df,shift_type_dict):
+    graph = MatchingGraph(df,shift_type_dict)
+    flow = graph.get_flow()
+    if flow is None:
+        avai_shifts = [get_shift_index(s) for s in shift_type_dict if len(shift_type_dict[s]) > 0]
+        for i in range(2):
+            retval = graph.get_best_flow_by_remove_shift(avai_shifts,i)
+            if retval is not None:
+                break
+
+    graph_copy = graph.copy()
+    for shift in retval[0]:
+        graph_copy.disable_shift(get_shift_index(shift))
+    flow = graph_copy.get_flow()
+
+    shift_list = graph_copy.get_shift_list(flow)
+
+    return shift_list
